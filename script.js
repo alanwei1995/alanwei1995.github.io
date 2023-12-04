6cd21fdde5addfdf43f0f3b2de02afc52311e679
Alan Wei <Alan.Wei@cnrl.com>
2023-12-04 13:19:22 -0700

    set up

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function changeText() {
-    document.getElementById("demo").innerHTML = "You clicked the button!";
-}
+document.getElementById('yahooLogin').addEventListener('click', function () {
+    var clientId = 'dj0yJmk9SW9NTEwyaEtXOGJOJmQ9WVdrOWJ6bHlkalZaWjIwbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWE1';
+    var redirectUri = encodeURIComponent('https://alanwei1995.github.io/');
+    var yahooAuthUrl = `https://api.login.yahoo.com/oauth2/request_auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
+
+    window.location.href = yahooAuthUrl;
+});
